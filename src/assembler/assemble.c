2694e7d1ebf8999ad25c73462158618548904c9e
Andrei Sologon <as5519@ic.ac.uk>
2020-06-14 14:08:17 +0300

    branching directives fixed

--- src/assembler/assemble.c
+++ src/assembler/assemble.c
@@ -18,70 +18,78 @@
 
 int main(int argc, char **argv) {
 
-    //1. verify the number of passed arguments
-    if (argc != 3) {
-        fprintf(stderr, "The number of provided arguments is not correct\n");
-        return EXIT_FAILURE;
-    }
+	//1. verify the number of passed arguments
+	if (argc != 3) {
+		fprintf(stderr, "The number of provided arguments is not correct\n");
+		return EXIT_FAILURE;
+	}
 
-    //2. initialize the source and output filenames
-    char *filename_source = argv[1];
-    char *filename_output = argv[2];
+	//2. initialize the source and output filenames
+	char *filename_source = argv[1];
+	char *filename_output = argv[2];
 
-    //3. load the assembly instructions
-    //4. generate symbol table
+	//3. load the assembly instructions
+	//4. generate symbol table
 
-    FILE *input;
-    if ((input = fopen(filename_source, "r")) == NULL) {
-        fprintf(stderr, "Error while opening the file\n");
-        exit(EXIT_FAILURE);
-    }
+	FILE *input;
+	if ((input = fopen(filename_source, "r")) == NULL) {
+		fprintf(stderr, "Error while opening the file\n");
+		exit(EXIT_FAILURE);
+	}
 
-    char line[MAX_LINE_LENGTH];
-    label_dict *dict = new_dict();
-    int count = 0;
+	char line[MAX_LINE_LENGTH];
+	label_dict *dict = new_dict();
+	int count = 0;
 
-    while (!feof(input)) {
-        if (fgets(line, MAX_LINE_LENGTH, input)) {
-            int n = strlen(line);
-            if (line[n - 1] == ':') {
-                line[n - 2] = '\0';
-                add(line, count, dict);
-            }
-            count++;
-        }
-    }
+	while (!feof(input)) {
+		char *p = fgets(line, MAX_LINE_LENGTH, input);
+		if(p == NULL) {
+			break;
+		}
+		int n = strlen(line);
+		if (line[n - 2] == ':') {
+			line[n - 2] = '\0';
+			add(line, count, dict);
+		}
+		count++;
+	}
 
-    // parse file again
-    if ((input = fopen(filename_source, "r")) == NULL) {
-        fprintf(stderr, "Error while opening the file\n");
-        exit(EXIT_FAILURE);
-    }
+	// parse file again
+	if ((input = fopen(filename_source, "r")) == NULL) {
+		fprintf(stderr, "Error while opening the file\n");
+		exit(EXIT_FAILURE);
+	}
 
-    FILE *output;
-    output = fopen(filename_output, "wb");
-    uint32_t binary;
-    Token token;
-    uint16_t address = 0;
+	FILE *output;
+	output = fopen(filename_output, "wb");
+	uint32_t binary;
+	Token token;
+	uint16_t address = 0;
 
-    while (!feof(input) && fgets(line, MAX_LINE_LENGTH, input)) {
-        int n = strlen(line);
-        printf("Length line: %d", n);
-        if (line[n - 2] == ':') {
-            continue;
-        }
-        printf("%s\n", line);
-        printf("%s\n", line);
-        parse_general(&token, line);
-        printf("dupa parser: \n");
-        printf("%x\n", binary);
-        instr_to_bits(&token, dict, address, &binary);
-        printf("dupa encoder: \n");
-        printf("%x\n", binary);
-        fwrite(&binary, sizeof(uint32_t), 1, output);
-        address += 4;
-    }
+	while (!feof(input)) {
+		char *p = fgets(line, MAX_LINE_LENGTH, input);
+		if(p == NULL) {
+			break;
+		}
+		int n = strlen(line);
+		line[n - 1] = '\0';
+		printf("Length line: %d", n);
+		if (line[n - 2] == ':') {
+			continue;
+		}
+		printf("%s\n", line);
+		printf("%s\n", line);
+		token.address = address;
+		parse_general(&token, line);
+		printf("dupa parser: \n");
+		printf("%x\n", binary);
+		instr_to_bits(&token, dict, address, &binary);
+		printf("dupa encoder: \n");
+		printf("%x\n", binary);
+		fwrite(&binary, sizeof(uint32_t), 1, output);
+		address += 4;
+	}
 
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
--- src/assembler/label_table.c
+++ src/assembler/label_table.c
@@ -1,32 +1,32 @@
 #include "label_table.h"
 
 label_dict *new_dict() {
-    label_dict *dict = malloc(sizeof(label_dict));
-    dict->length = 0;
-    return dict;
+	label_dict *dict = malloc(sizeof(label_dict));
+	dict->length = 0;
+	return dict;
 }
 
 void add(const char *label, uint16_t address, label_dict *dict) {
-    dict->length++;
-    dict->labels = realloc(dict->labels, dict->length);
-    label_pair *pair = malloc(sizeof(label_pair));
-    pair->address = address;
-    strcpy(pair->label, label);
-    dict->labels[dict->length - 1] = pair;
+	dict->length++;
+	dict->labels = realloc(dict->labels, dict->length);
+	label_pair *pair = malloc(sizeof(label_pair));
+	pair->address = address;
+	strcpy(pair->label, label);
+	dict->labels[dict->length - 1] = pair;
 }
 
 uint16_t query(const char *label, label_dict *dict) {
-    for (int i = 0; i < dict->length - 1; i++) {
-        if (strcmp(label, dict->labels[i]->label) == 0) {
-            return dict->labels[i]->address;
-        }
-    }
-    return 0;
+	for (int i = 0; i < dict->length; i++) {
+		if (strcmp(label, dict->labels[i]->label) == 0) {
+			return dict->labels[i]->address;
+		}
+	}
+	return 0;
 }
 
 void free_dict(label_dict *dict) {
-    for (int i = 0; i < dict->length - 1; i++) {
-        free(&dict->labels[i]);
-    }
-    free(dict);
+	for (int i = 0; i < dict->length - 1; i++) {
+		free(&dict->labels[i]);
+	}
+	free(dict);
 }
